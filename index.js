352793b593ca78b2427994ebd8caf2a7f9e26d4b
Neven Bedr <neven2225@gmail.com>
2020-05-20 12:25:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ function unleashDog(dogName, dogBreed){
 return `Unleash ${dogName} the ${dogBreed}`;
 }
 const routine=[wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog];
+const result=[];
 function exerciseDog(dogName,dogBreed){
-  const result=[];
+  
   for( let i=0;i<routine.length;i++){
  
 }
